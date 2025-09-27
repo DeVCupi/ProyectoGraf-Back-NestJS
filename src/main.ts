9cfa7c526b7fe5b69f97e3b0cc80045f676fb85c
Jose Manuel Sanchez Osma <josmsanchez11@misena.edu.co>
2025-09-26 22:01:31 -0500

    sincronizar los valores de .env nueva base datos prod

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,11 @@ import { ResponseInterceptor } from './common/interceptors/response.interceptor'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+  origin: ['http://localhost:4200', 'https://miapp.com'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+});
   app.useGlobalInterceptors(new ResponseInterceptor());
   await app.listen(3000);
 }
