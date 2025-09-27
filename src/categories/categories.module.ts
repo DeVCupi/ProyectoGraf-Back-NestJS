b4a9ab9a02331819c17cdcbaed3751cacd0ea57d
Jhoan Cupitre <jhoan.cupitre@co.ey.com>
2025-09-26 20:27:45 -0500

    1:Fix: Cambio de rutas relativas 2

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -4,8 +4,8 @@ import { CategoriesController } from './categories.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Category } from './entities/category.entity';
 import { Subcategory } from './entities/subcategory.entity';
-import { ScraperModule } from 'src/scraper/scraper.module';
-import { ProductsModule } from 'src/products/products.module';
+import { ScraperModule } from '../scraper/scraper.module';
+import { ProductsModule } from '../products/products.module';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Category, Subcategory]), ScraperModule, ProductsModule],
